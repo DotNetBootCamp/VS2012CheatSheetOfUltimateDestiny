08058b6fa1bfa0454f2e5e1dcf1f647793aae344
Ryan Kotzen <Ryank@entelect.co.za>
2015-02-07 16:12:48 +0200

    Adding more images

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -7,6 +7,10 @@ namespace intro
 {
     public class HelloWorld
     {
+        public HelloWorld()
+        {
+            var _legacyLoader = LegacyCodeDoTheThing();
+        }
         public string DoTheThing()
         {
             return "Hello World!";
@@ -17,5 +21,23 @@ namespace intro
             return string.Format("Hello World! {0}", stuff);
         }
 
+        public string SlightlyMoreComplicatedThing(int value)
+        {
+            var moddedValue = value % 3;
+            switch (moddedValue)
+            {
+                case 0: return "Hello";
+                case 1: return " ";
+                case 2: return "World!";
+                default:
+                    throw new ArgumentOutOfRangeException(string.Format("Modded value {0} was not catered for. Don't you just hate using a switch statement?", moddedValue));
+            }
+        }
+
+        [Obsolete]
+        public string LegacyCodeDoTheThing()
+        {
+            return "World!Hello";
+        }
     }
 }
