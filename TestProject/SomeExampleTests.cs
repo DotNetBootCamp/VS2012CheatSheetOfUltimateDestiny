ae7895a4564f9d68376e96c4c31bcdbdfe718c4e
Ryan Kotzen <Ryank@entelect.co.za>
2015-02-08 21:14:33 +0200

    reverting the deletion of the TestProject

---
+++ TestProject/SomeExampleTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using intro;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject
+{
+    [TestClass]
+    public class SomeExampleTests
+    {
+        [TestMethod]
+        public void Ensure1equals1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void EnsureHelloWorldIsEqual()
+        {
+            var helloWorld = new HelloWorld();
+            StringAssert.Equals("Hello World!", helloWorld.DoTheThing());
+        }
+
+        [TestMethod]
+        public void EnsureHelloWorldIsEqualAndStuff()
+        {
+            var helloWorld = new HelloWorld();
+            var stuff = "potato1";
+            StringAssert.Equals(string.Format("Hello World!{0}", stuff), helloWorld.DoTheThingWithTheStuff(stuff));
+        }
+    }
+}
